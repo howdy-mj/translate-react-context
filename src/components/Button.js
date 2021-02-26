15c38f121878d044a340e7799c94decb336e8116
MJ <hi.minjungkim@gmail.com>
2021-02-26 23:56:44 +0900

    Create and Use Context

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,9 +1,32 @@
 import React from 'react';
+import LanguageContext from '../contexts/LanguageContext';
+import ColorContext from '../contexts/ColorContext';
 
 class Button extends React.Component {
+  // static contextType = LanguageContext;
+  renderSubmit(value) {
+    return value === 'english' ? 'Submit' : 'Voorleggen';
+  }
+
   render() {
-    return <button className="ui button primary">Submit</button>;
+    // const text = this.context === 'english' ? 'Submit' : 'Voorleggen';
+    // return <button className="ui button primary">{text}</button>;
+
+    return (
+      <ColorContext.Consumer>
+        {(color) => (
+          <button className={`ui button ${color}`}>
+            <LanguageContext.Consumer>
+              {(language) => this.renderSubmit(language)}
+            </LanguageContext.Consumer>
+          </button>
+        )}
+      </ColorContext.Consumer>
+    );
   }
 }
+
+// 이렇게 할 수도 있음
+// Button.contextType = LanguageContext;
 
 export default Button;
